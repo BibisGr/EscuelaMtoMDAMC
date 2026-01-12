15bd6c61954386de7aee6e5e623a5254159c5375
Bibis Gr <bertha.garrastacho@digitechfp.com>
2026-01-12 17:19:40 +0100

    Initial commit

---
+++ src/main/java/progresa/escuelamtom/EscuelaMtoMApplication.java
@@ -0,0 +1,13 @@
+package progresa.escuelamtom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscuelaMtoMApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EscuelaMtoMApplication.class, args);
+    }
+
+}
---
+++ src/test/java/progresa/escuelamtom/EscuelaMtoMApplicationTests.java
@@ -0,0 +1,13 @@
+package progresa.escuelamtom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscuelaMtoMApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
